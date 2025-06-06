3507ae620768cd1dd8eaf5a3a9befe87794a8d3e
LinLai <zzbsn.cn@outlook.com>
2025-06-06 19:31:59 +0800

    update base

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -14,7 +14,7 @@ import { defineUserConfig } from 'vuepress'
 import { plumeTheme } from 'vuepress-theme-plume'
 
 export default defineUserConfig({
-  base: '/',
+  base: '/vuepress_plume/',
   lang: 'zh-CN',
   locales: {
     '/': {
