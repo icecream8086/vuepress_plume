926292d294295c874440a538b2030a38b3f283fc
LinLai <zzbsn.cn@outlook.com>
2025-06-08 00:39:28 +0800

    2

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -50,7 +50,7 @@ export default defineUserConfig({
 
   theme: plumeTheme({
     /* 添加您的部署域名, 有助于 SEO, 生成 sitemap */
-    // hostname: 'https://your_site_url',
+    hostname: 'https://www.hexgrid.ink',
 
     /* 文档仓库配置，用于 editLink */
     // docsRepo: '',
