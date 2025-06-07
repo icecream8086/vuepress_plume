3dc80937c3d4f33e720c0631919904946b512daa
LinLai <zzbsn.cn@outlook.com>
2025-06-07 17:18:28 +0800

    2

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -17,7 +17,7 @@ import { viteBundler } from '@vuepress/bundler-vite'
 import { defineUserConfig } from 'vuepress'
 import { plumeTheme } from 'vuepress-theme-plume'
 
-const DEBUG = true
+const DEBUG = false
 const BASE_PATH = 'icecream8086'
 const BASE_URL = DEBUG ? '/' : `/${BASE_PATH}/`
 // const BASE_URL = '/'
