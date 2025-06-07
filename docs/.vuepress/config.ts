26317498d0fa9709beb62af4ad97708f59afb5b2
LinLai <zzbsn.cn@outlook.com>
2025-06-08 00:52:43 +0800

    2

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -50,7 +50,7 @@ export default defineUserConfig({
 
   theme: plumeTheme({
     /* 添加您的部署域名, 有助于 SEO, 生成 sitemap */
-    hostname: 'https://www.hexgrid.ink',
+    hostname: 'https://icecream8086.github.io',
 
     /* 文档仓库配置，用于 editLink */
     // docsRepo: '',
